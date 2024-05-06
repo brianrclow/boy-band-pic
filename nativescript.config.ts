1abfb7d6e521f34f6e0b7867b3d9ea14e557865f
brianrclow <brianrclow@gmail.com>
2024-05-06 16:52:07 -0600

    update for initial testflight

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.boybandpic',
+  id: 'com.brianrclow.boybandpic',
   appPath: 'src',
   appResourcesPath: 'App_Resources',
   android: {
